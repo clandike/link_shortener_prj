bb5761ba3f2cd7df9783cf83e733c07f832d85ff
Sviatoslav <slavaua2312@gmail.com>
2025-09-14 16:05:30 +0300

    implemented non-autorized users logic

--- obj/Debug/net8.0/UrlShortener.AssemblyInfo.cs
+++ obj/Debug/net8.0/UrlShortener.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb30fbdb88909d128fb01b6a655f0a60cbe1ff3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72c9a6a01cddbe271ed8a60688c46ba09a09b0ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
